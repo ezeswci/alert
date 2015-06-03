fd322521d8c1e0b996da6f86815e79c8a355f134
ezeswci <ezequiel@swci.com.ar>
2015-06-03 13:27:03 -0300

    Revert "va"
    This reverts commit 0d4ebab684bba0d6360cbc03139b8e2a8138ddaa.

--- js/importarDeAgenda.js
+++ js/importarDeAgenda.js
@@ -19,7 +19,6 @@ function agregarDesdeLaAgenda2(nombre,mails,telefonos){
 	document.getElementById("cartel3").style.visibility="visible";
 }
 function agregarDesdeLaAgenda(){
-	alert("va");
 	navigator.contacts.pickContact(function(contact){
 		result="Datos:";
 		mails=new Array();
