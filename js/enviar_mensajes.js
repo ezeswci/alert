8f78e7ab52d16b2ce701ad9eda7f1fde844a90fd
ezeswci <ezequiel@swci.com.ar>
2015-06-04 11:47:07 -0300

    mensajes

--- js/enviar_mensajes.js
+++ js/enviar_mensajes.js
@@ -1,7 +1,61 @@
 // JavaScript Document
 function enviarMensajes(){
 	alert("Envio los mensajes");
+	salenMensajes();
 }
 function enviarMensajesApagadoFalse(){
 	alert("Envio los mensajes de apagado Falso");
+}
+function salenMensajes() {
+	var dbSize = 200000;
+    var dbName = "TMD";
+    var dbVersion = "1.0";
+    var dbDisplayName = "TMDDatabase";
+	//alert("Empieza");
+    //Init DB
+    //
+    window.db = window.openDatabase(dbName, dbVersion, dbDisplayName, dbSize);
+    db.transaction(selectMsj, errorCB);
+}
+function selectMsj(tx) {
+    tx.executeSql('SELECT * FROM CONTACT', [], querySuccessM, errorCB);
+}
+
+function querySuccessM(tx, rs) {
+    // this will be empty since no rows were inserted.
+	var mails= new Array();
+    for (var i = 0; i < rs.rows.length; i++) {
+        var p = rs.rows.item(i);
+		if(con_tipo=="Mail"){mails.push(p.con_destino);}else{
+        eviarMensaje(p.con_id, p.con_tipo, p.con_nombre, p.con_destino);}
+        //alert(element);
+    }
+	if(mails.length>0){
+		enviarMails(mails);
+	}
+}
+function eviarMensaje(con_id, con_tipo, con_nombre, con_destino){
+	message="Esto es un Mensaje de la App Tu Alerta entra en este link para seguirme: http://www.swci.com.ar/monitoreo/vehiculos_posicion_direct.php?gps="+device.uuid;
+		var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }}
+		var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(con_destino, message, options, success, error);	
+}
+function errorCB(){
+	// Por ahora no hace nada pero podria reactivar los mensajes
+}
+function enviarMails(destinos) {
+	msj="Esto es un Mensaje de la App Tu Alerta entra en este link para seguirme: http://www.swci.com.ar/monitoreo/vehiculos_posicion_direct.php?gps="+device.uuid;
+	window.plugin.email.open({
+    to:      destinos,
+    cc:      [''],
+    bcc:     [''],
+    subject: 'Mensaje de la App Mi Alerta',
+    body:    msj
+});
 }
