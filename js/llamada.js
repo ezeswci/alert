63832f9cffd58c9d3811dd50da390bd7ce24d81e
ezeswci <ezequiel@swci.com.ar>
2015-06-04 14:25:59 -0300

    llamada

--- js/llamada.js
+++ js/llamada.js
@@ -4,6 +4,7 @@ function apretoLlamada(elemento){
 	if(elemento.src.indexOf("llamada_desactivada")!=-1){
 		elemento.src="img/llamada_activada.jpg";
 		alert("Llamada Activada");
+		document.location.href = 'tel:+01148127101';
 	}else{
 		elemento.src="img/llamada_desactivada.jpg";
 		alert("Llamada Desactivada");
